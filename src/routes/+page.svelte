38782be2c2c745e123441211eaf8b2d49b97cf15
Cameron Avelis <cmavelis@gmail.com>
2025-01-24 19:30:05 -0800

    add grid to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    const map = $state(Array(50).fill(0).concat(Array(50).fill(1)))
+</script>
+
+<div class="grid">
+    {#each map as cell, i}
+        <div class={{box:true, color: cell}}>{cell} {i}</div>
+    {/each}
+</div>
+
+<style>
+    .grid {
+        display: grid;
+        grid-template-columns: repeat(10, min-content);
+        gap: 2px
+    }
+    .box {
+        width: 50px;
+        height: 50px;
+        background-color: slategray;
+    }
+    .color {
+        background-color: #583c36;
+    }
+</style>
