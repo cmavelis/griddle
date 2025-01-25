b88865463715b949c447cea626425da22cc82b53
Cameron Avelis <cmavelis@gmail.com>
2025-01-24 21:28:58 -0800

    working grid for 3 colors, click and drag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,53 @@
-<script>
+<script lang="ts">
     const map = $state(Array(50).fill(0).concat(Array(50).fill(1)))
     const lockedCell = 50
+    let drawing = $state(false)
+    let drawValue = $state(0)
+    const maxDrawValue = 1
+    const rightClickValue = maxDrawValue + 1
+
+    const getIdFromEvent = (e) => {
+        const {cmaBoxId} = e.target.dataset
+        if (cmaBoxId !== String(lockedCell)) {
+            return cmaBoxId
+        }
+        return false
+    }
+
+    const handleGridMouseDown = (e) => {
+        e.preventDefault()
+        const id = getIdFromEvent(e)
+        if (id) {
+            const newDrawValue = map[id] + 1
+            drawValue = newDrawValue > maxDrawValue ? 0 : newDrawValue
+            map[id] = drawValue
+            drawing = true
+        }
+    }
+
+    const handleMouseEnter = (e) => {
+        const id = getIdFromEvent(e)
+        if (drawing && id)
+        {
+            map[id] = drawValue
+        }    }
+
+    const handleMouseUp = () => {
+        drawing = false
+    }
+
+    const handleRightClick = (e) => {
+        e.preventDefault()
+        const id = getIdFromEvent(e)
+        if (id) {
+            map[id] = rightClickValue
+        }
+    }
 </script>
 
-<div class="grid">
+<div class="grid" onmousedown={handleGridMouseDown} onmouseover={handleMouseEnter} onmouseup={handleMouseUp} oncontextmenu={handleRightClick}>
     {#each map as cell, i}
-        <div class={{box:true, color: cell, locked: i === lockedCell}}>{cell} {i}</div>
+        <div data-cma-box-id={i} data-cma-box-value={cell} class={['box', `color${cell}`, i === lockedCell && 'locked']}>{cell} {i}</div>
     {/each}
 </div>
 
@@ -20,8 +62,11 @@
         height: 50px;
         background-color: slategray;
     }
-    .color {
+    .color1 {
         background-color: #583c36;
+    }
+    .color2 {
+        background-color: hotpink;
     }
     .locked {
         outline: 2px solid red
